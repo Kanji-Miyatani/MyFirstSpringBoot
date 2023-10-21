ec926771eca31bfbd0f28b198e69c07a9eb78e21
Kanji Miyatani <kanji09012458@gmail.com>
2023-10-21 20:35:34 +0900

    initial commit

---
+++ myspringboot/src/main/java/com/example/myspringboot/SpringboardApplication.java
@@ -0,0 +1,19 @@
+package com.example.myspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringboardApplication.class, args);
+	}
+	@RequestMapping("/hello")
+	String index(){
+		return "Hello World!";
+	}
+}
