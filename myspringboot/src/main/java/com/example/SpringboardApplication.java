eab50f9701d3f685f279987e13dea122dd07e0d7
Kanji Miyatani <kanji09012458@gmail.com>
2023-10-24 19:24:16 +0900

    ホーム画面html追加

--- myspringboot/src/main/java/com/example/SpringboardApplication.java
+++ myspringboot/src/main/java/com/example/SpringboardApplication.java
@@ -6,14 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class SpringboardApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringboardApplication.class, args);
-	}
-	@RequestMapping("/hello")
-	String index(){
-		return "Hello World!";
 	}
 }
--- myspringboot/src/main/java/com/example/controller/ProductController.java
+++ myspringboot/src/main/java/com/example/controller/ProductController.java
@@ -3,11 +3,12 @@ package com.example.controller;
 import com.example.model.Product;
 import com.example.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 
 import java.util.List;
-
+@Controller
 public class ProductController {
     // 商品操作サービスのインスタンス（実体）を作ります。このサービスが実際の商品操作を行います。
     private final ProductService productService;
