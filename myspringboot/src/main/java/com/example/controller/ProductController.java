3005f9deeaecd10977f4cb247991c92b53cf59b2
Kanji Miyatani <kanji09012458@gmail.com>
2023-10-31 23:32:30 +0900

    404の原因はpom.xmlのArtifactID

--- myspringboot/src/main/java/com/example/controller/ProductController.java
+++ myspringboot/src/main/java/com/example/controller/ProductController.java
@@ -6,13 +6,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 @Controller
 public class ProductController {
     @GetMapping("/")
     @RequestMapping(method = RequestMethod.GET)
-    public String showHomePage(){
-        return "index";
+    public ModelAndView showHomePage(){
+        ModelAndView model = new ModelAndView();
+        model.setViewName("index");
+        return model;
     }
 }
